eb679c6eb7eb059545b50d80d090e60381d900b3
Zeinab Jeylani <zeinabjeylani0@gmail.com>
2020-01-09 17:17:44 -0600

    add password generator

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+//generate random password
 
+function generate () {
+
+    let complexity = document.getElementById("slider").value
+
+    let values= "ABCDERFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
+
+    let password= "";
+}
